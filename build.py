0692df229c0de0095c2c1708c3c8b90a3459eb7b
matthieu.royer_de_vericourt <matthieu.royer_de_vericourt@mines-paristech.fr>
2021-01-29 11:28:10 +0100

    un peu de build

---
+++ build.py
@@ -0,0 +1,26 @@
+# from character import Character
+import pygame as pg
+import affichage as aff
+from Map import Map
+
+
+
+class Game:
+
+    def __init__(self):
+        self.char_name = input("What is your name, Stranger?")
+        print(f"Welcome {self.char_name}")
+        #self.main_char = Character(self.char_name)
+        self.niveau = 1
+        self.map = Map()
+    
+    def affichage(self):
+        aff.affichage(map)
+    
+    def process(self):
+        self.affichage()
+
+Game()
+    
+    
+    
