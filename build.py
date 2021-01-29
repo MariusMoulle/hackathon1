d7b91575b1259a8d41b21b3ff8c5d5bd884aa87a
matthieu.royer_de_vericourt <matthieu.royer_de_vericourt@mines-paristech.fr>
2021-01-29 12:40:21 +0100

    build

--- build.py
+++ build.py
@@ -1,25 +1,46 @@
-# from character import Character
+from character import Character
 import pygame as pg
 import affichage as aff
 from Map import Map
 
-
+pg.init()
 
 class Game:
 
     def __init__(self):
         self.char_name = input("What is your name, Stranger?")
         print(f"Welcome {self.char_name}")
-        #self.main_char = Character(self.char_name)
+        self.character = Character(self.char_name)
         self.niveau = 1
         self.map = Map()
-        self.process()
+        self.play()
     
     def affichage(self):
         aff.affichage(self.map)
     
-    def process(self):
-        self.affichage()
+    def play(self):
+        running = True
+        while running:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    running = False
+                elif event.type == pg.KEYDOWN:
+                    if event.key == pg.K_q:
+                        running = False
+                    elif event.key == pg.K_UP :
+                        direction=(0,-1)
+                    elif event.key == pg.K_DOWN :
+                        direction=(0,1)
+                    elif event.key == pg.K_RIGHT :
+                        direction=(1,0)
+                    elif event.key == pg.K_LEFT :
+                        direction=(-1,0)
+            
+            self.character.movement(direction)
+            
+            
+
+            
 
 Game()
     
