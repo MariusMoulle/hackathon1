d2ab6b1a94acfe74dd22961e60d78f06a335c68e
matthieu.royer_de_vericourt <matthieu.royer_de_vericourt@mines-paristech.fr>
2021-01-29 14:44:41 +0100

    on avaaaaaaance

--- build.py
+++ build.py
@@ -8,22 +8,49 @@ pg.init()
 class Game:
 
     def __init__(self):
-        self.char_name = input("What is your name, Stranger?")
+        self.char_name = input("What is your name, Stranger? ")
         print(f"Welcome {self.char_name}")
-        self.character = Character(self.char_name)
-        self.niveau = 1
         self.map = Map()
+        self.character = Character(self.char_name, self)
         self.play()
     
-    def affichage(self):
-        aff.affichage(self.map)
+    def affiche(self):
+        S = self.grille
+        print(''.join([''.join(S[i]) + '\n' for i in range(len(S))]))
     
     def combine(self):
-        pass
+        # n, p = self.map.dims
+        n, p = 30, 30
+        S = [[' ' for j in range(p)] for i in range(n)]
+        for sol in self.map.quadrillage_sol():
+            i, j = sol
+            S[i][j] = '.'
+
+        for chemin in self.map.chemins():
+            i, j = chemin
+            S[i][j] = '#'
+        
+        for porte in self.map.portes():
+            i, j = porte
+            S[i][j] = '+'
+        
+        i, j = self.character.position
+        S[i][j] = '@'
+
+        for objet in self.map.objets():
+            if objet.position == self.character.position:
+                self.map.remove(objet)
+                self.character.inventaire[objet.type] += objet.valeur
+            i, j = objet.position
+            S[i][j] = objet.repr
+        
+        self.grille = S
+        
 
     def play(self):
         running = True
         while running:
+            direction = (0,0)
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
@@ -40,8 +67,8 @@ class Game:
                         direction=(-1,0)
             
             self.character.movement(direction)
-            schema = self.combine(self.map, self.caracter.position)
-            self.affiche(schema)
+            self.combine()
+            self.affiche()
 
 
             
--- character.py
+++ character.py
@@ -4,43 +4,43 @@ import numpy as np
 # On crée un dictionnaire pour associer les touches appuyées à la commande qu'on veut
 # Ex : si on appuie sur la flèche du haut, on voudrait que python comprenne que le personnage veut monter
 
-Touches = {K_LEFT:37, K_UP:38, K_RIGHT:39, K_DOWN:40}
+# Touches = {K_LEFT:37, K_UP:38, K_RIGHT:39, K_DOWN:40}
 
 class Character:
 
-    def __init__(self, name:str, position:np.array, game):
+    def __init__(self, name:str, game, position = (0,0)):
         self.name = name
         self.position = position # un array contenant l'indice des lignes et celui des colonnes
-        self.inventaire = {armor:5, arms:10, life:10, gold:50, food:5, water:5}
+        self.inventaire = {'armor':5, 'arms':10, 'life':10, 'gold':50, 'food':5, 'water':5}
         self.repr = '@'
         self.game = game
 
-    def movement(self, direction:np.array): 
-        dx,dy = direction # un tuple qui contient la direction
+    def movement(self, direc:tuple): 
+        dx,dy = direc # un tuple qui contient la direction
         
-        new_pos = (self.position[0] + dx, self.position[1] + dy)
+        new_pos = [self.position[0] + dx, self.position[1] + dy]
 
-        if self.position in self.game.map.quadrillage_sol:
+        if new_pos in self.game.map.quadrillage_sol():
             self.position[0] += dx
             self.position[1] += dy
             
-            i, j = self.position
-            if S[i][j] == 'a':
-                self.inventaire(food) += 1
-            elif S[i][j] == 'w':
-                self.inventaire(water) += 1
-            elif S[i][j] == 'A':
-                self.inventaire(armor) += 1
-            elif S[i][j] == 'T':
-                self.inventaire(arms) += 1
-            elif S[i][j] == 'o':
-                self.inventaire(food) += 5
+            # i, j = self.position
+            # if S[i][j] == 'a':
+            #     self.inventaire(food) += 1
+            # elif S[i][j] == 'w':
+            #     self.inventaire(water) += 1
+            # elif S[i][j] == 'A':
+            #     self.inventaire(armor) += 1
+            # elif S[i][j] == 'T':
+            #     self.inventaire(arms) += 1
+            # elif S[i][j] == 'o':
+            #     self.inventaire(food) += 5
 
-            elif S[i][j] == 'B':
-                win = np.random.randint(1)
-                if win == 0:
-                    self.inventaire(life) -= 5
-                else : 
-                    self.inventaire(life) += 5  
+            # elif S[i][j] == 'B':
+            #     win = np.random.randint(1)
+            #     if win == 0:
+            #         self.inventaire(life) -= 5
+            #     else : 
+            #         self.inventaire(life) += 5  
             
-            S[i][j] = '@'
+            # S[i][j] = '@'
